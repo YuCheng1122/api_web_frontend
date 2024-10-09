780430b5787b851f39dfed50bf2b4ffc52584368
YuCheng1122 <tommymask00@gmail.com>
2024-10-09 02:12:11 +0000

    Refactor script management and update graph page
    - Updated  and  for improved script handling
    - Modified  for graph page enhancements
    - Removed deprecated
    - Updated  and  for dependencies
    - Added new assets:  and
    
    This commit focuses on refining script management features, improving the graph page, and removing outdated components.

--- src/app/admin/script/page.tsx
+++ src/app/admin/script/page.tsx
@@ -1,27 +1,14 @@
 "use client"; // 標記為客戶端組件
 
-import React, { useState } from 'react';
+import React from 'react';
 import ScriptDownloadForm from '../../../components/admin/script/ScriptDownloadForm'; // 引入表單組件
-import LicensePage from '../../../components/admin/script/License'; // 引入表單組件
 
 const SignupPage = () => {
-    const [isLicenseValid, setIsLicenseValid] = useState(false); // 狀態來控制是否驗證成功
-
-    const handleLicenseValidation = (isValid: boolean) => {
-        setIsLicenseValid(isValid); // 更新狀態，依據 License 驗證結果決定是否顯示 ScriptDownloadForm
-    };
-
     return (
         <div className="flex flex-col rounded-lg items-center justify-center">
-            {!isLicenseValid ? (
-                <div>
-                    <div className="h-[18vh]"></div>
-                    <LicensePage onLicenseValidated={handleLicenseValidation} /> {/*傳遞回調函數*/}
-                </div>
-                
-            ) : (
-                <ScriptDownloadForm /> // License 驗證通過後顯示表單
-            )}
+            {/* 直接顯示 ScriptDownloadForm */}
+            <ScriptDownloadForm /> 
+            {/* 其他代碼已省略 */}
         </div>
     );
 };
