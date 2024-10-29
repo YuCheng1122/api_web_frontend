4e60d6104a12c65a16ba40cffbee73b17132a8ce
jiweihong0 <jiweihong0@gmail.com>
2024-10-29 10:03:31 +0800

    fix: data

--- src/components/dashboard/DateTimeFilter.tsx
+++ src/components/dashboard/DateTimeFilter.tsx
@@ -60,12 +60,12 @@ const DateTimeFilter = () => {
   };
 
   return (
-    <div className="flex flex-wrap justify-between ">
+    <div className="flex flex-wrap justify-around w-screen ">
       <div className=''>
         <label htmlFor="start-date" className="block text-sm font-medium text-gray-700 px-1 mb-2">開始時間</label>
         <input
           name='start-date'
-          className='p-2 rounded-lg shadow-lg border border-gray-300 min-w-80'
+          className='p-2 rounded-lg shadow-lg border border-gray-300 min-w-80 w-full'
           type='datetime-local'
           value={startDate}
           onChange={(e) => setStartDate(e.target.value)}
@@ -85,7 +85,7 @@ const DateTimeFilter = () => {
       <button
         type="button"
         onClick={handleNowClick}
-        className="ml-2 px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
+        className="mt-5 min-w-20 max-h-10 ml-2 px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
       >
         Now
       </button>
@@ -93,7 +93,7 @@ const DateTimeFilter = () => {
       <button
         onClick={handleSubmit}
         disabled={isLoading}
-        className={`px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}
+        className={`min-w-48 md:min-w-[600px] px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}
       >
         {isLoading ? 'Loading...' : '取得數據'}
       </button>
