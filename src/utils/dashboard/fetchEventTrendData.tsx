e85aa174ed3d969be370442d7c552ecdfcf7b379
jiweihong0 <jiweihong0@gmail.com>
2024-10-31 01:21:06 +0800

    feat: all error

--- src/utils/dashboard/fetchEventTrendData.tsx
+++ src/utils/dashboard/fetchEventTrendData.tsx
@@ -1,26 +1,31 @@
 import axios from 'axios'
+import { log } from 'console';
 import Cookies from 'js-cookie'
 
 const normalizeData = (data: number[]): number[] => {
   const min = Math.min(...data);
   const max = Math.max(...data);
   const range = max - min;
-  
+
   if (range === 0) return data;
 
   return data.map(value => {
     const normalizedValue = (value - min) / range;
     const compressionFactor = 0.5;
-    return min + (normalizedValue ** compressionFactor) * range;
+    const calulatedValue = min + (normalizedValue ** compressionFactor) * range;
+    // to Int
+    const calulatedValueInt = Math.round(calulatedValue);
+    return calulatedValueInt;
+
   });
 };
 
 export interface fetchEventTrendDataType {
-  label: string[]  
+  label: string[]
   datas: {
     name: string
     type: string
-    data: Array<[string, number]> 
+    data: Array<[string, number]>
   }[]
 }
 
@@ -35,24 +40,24 @@ export interface fetchEventTrendDataResponse {
 }
 
 export const initData: fetchEventTrendDataType = {
-  label: [],  
+  label: [],
   datas: []
 }
 
 export const fetchEventTrendData = async (param: fetchEventTrendDataRequest): Promise<fetchEventTrendDataResponse> => {
   const api_url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/wazuh/line-chart`;
-  
+
   try {
     const header = {
       'Authorization': Cookies.get('token')
     }
-    
+
     const response = await axios.get(api_url, {
       params: {
         start_time: param.start.toISOString(),
         end_time: param.end.toISOString()
       },
-      headers: header        
+      headers: header
     });
 
     const apiData = response.data;
@@ -63,7 +68,9 @@ export const fetchEventTrendData = async (param: fetchEventTrendDataRequest): Pr
     const result: fetchEventTrendDataType = {
       label: apiData.label || [],
       datas: apiData.datas.map((dataset: any) => {
-        const values = dataset.data.map(([_, value]: [string, number] ) => value);
+        const values = dataset.data.map(([_, value]: [string, number]) => value);
+        console.log('values:', values);
+
         const normalizedValues = normalizeData(values);
         return {
           ...dataset,
@@ -71,6 +78,8 @@ export const fetchEventTrendData = async (param: fetchEventTrendDataRequest): Pr
         };
       })
     };
+    console.log('result:', result);
+
 
     return {
       success: true,
